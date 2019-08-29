aba1c3369f37cf3ec4df127d20b1309ffb673439
whenofficial <fsmitchm@gmail.com>
2019-08-29 18:32:57 +0800

    Inferno 1.2.0

--- src/util/Instance.ts
+++ src/util/Instance.ts
@@ -2,5 +2,5 @@ export var instance: object = {
     app: undefined
 };
 
-export const VERSION = '1.1.3';
-export const RELEASE_DATE = '28 August 2019';
+export const VERSION = '1.2.0';
+export const RELEASE_DATE = '29 August 2019';
