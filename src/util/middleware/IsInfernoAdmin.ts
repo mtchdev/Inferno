9e6ba8869a2cf1ef47d793e3fb9b2b5fa92489c7
whenofficial <fsmitchm@gmail.com>
2020-04-28 21:33:02 +0800

    Revise admin list

--- src/util/middleware/IsInfernoAdmin.ts
+++ src/util/middleware/IsInfernoAdmin.ts
@@ -2,10 +2,9 @@ import { IPluginMiddleware } from './Middleware';
 import { Message, Client } from 'discord.js';
 
 const InfernoAdminIds: Array<string> = [
-    '308550329216270338',
-    '543891397737054212',
-    '396487205079482378',
-    '408524061325656065'
+    '308550329216270338', // mitch#3071
+    '376068799121719300', // Luca#6815
+    '408524061325656065' //Simon.#0001
 ];
 
 export class IsInfernoAdmin implements IPluginMiddleware {
